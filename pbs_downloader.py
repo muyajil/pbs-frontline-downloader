71f927eb4d0ba3c846b936945fc23f6dab10e37f
Mohammed Ajil <mohammed@ajil.ch>
2020-07-03 19:32:21 +0200

    add root dir argument, handle errors when downloading

--- pbs_downloader.py
+++ pbs_downloader.py
@@ -2,6 +2,7 @@ import subprocess
 import requests
 from bs4 import BeautifulSoup
 import os
+import argparse
 
 BASE_URL = "https://www.pbs.org/wgbh/frontline/films/"
 
@@ -20,11 +21,10 @@ def get_season_and_episode(film_url):
 
 def get_film_links(base_url):
     num_pages = get_num_pages(base_url)
-    for page in range(num_pages, 0, -1):
+    for page in range(0, num_pages + 1):
         result_content = requests.get(f"{base_url}page/{page}").text
         soup = BeautifulSoup(result_content, features='lxml')
         film_blocks = soup.find_all("div", {"class": "list__item"})
-        film_blocks.reverse()
         for film_block in film_blocks:
             try:
                 url_item = film_block.find_all("a", {"class": "black-playhead-outer"})[0]
@@ -42,13 +42,21 @@ def get_film_links(base_url):
             )
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('root_dir', metavar='root_dir', type=str, nargs=1, help='root dir where to store videos')
+    args = parser.parse_args()
+    errors_file = open('errors.log', 'w')
     film_links = get_film_links(BASE_URL)
     for title, url, season, episode in film_links:
-        season_folder = "Season " + season
+        season_folder = os.path.join(args.root_dir, "Season " + season)
         output_file_name = f"{season_folder}/Frontline - S{season}E{episode:0>2} - {title}.mp4"
         print(season, episode, title, output_file_name, url)
         if not os.path.exists(season_folder):
             os.makedirs(season_folder)
         command = ["youtube-dl", "-o", output_file_name, url]
         result = subprocess.run(command)
-        result.check_returncode()
+        try:
+            result.check_returncode()
+        except:
+            errors_file.write(f'ERROR: {season} {episode} {title} {url}')
+    errors_file.close()
